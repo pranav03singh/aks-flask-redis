200a4d8ff41b25101e05302fe51471a8ccd2ea37
pranav03singh <44692624+pranav03singh@users.noreply.github.com>
2022-03-27 17:16:39 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,5 +9,9 @@ terraform {
 
 provider "azurerm" {
   features {}
+  subscription_id = "00000000-0000-0000-0000-000000000000"
+  client_id       = "00000000-0000-0000-0000-000000000000"
+  client_secret   = "xxx"
+  tenant_id       = "00000000-0000-0000-0000-000000000000"
   # Configuration options
 }
